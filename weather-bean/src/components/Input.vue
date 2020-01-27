8a687aed68a1a5906f1874ebf6e7aadb9d93ccdd
Jenny Le <jennyledangg@gmail.com>
2020-01-27 07:32:46 -0600

    My thought process

--- weather-bean/src/components/Input.vue
+++ weather-bean/src/components/Input.vue
@@ -1,5 +1,7 @@
 <template>
 <div>
+    <!-- I created an input field that would take info and track what the user was typing
+    then when the user clicks the button to send in the value. It would activae sendData Method" -->
   <input 
   placeholder="Enter a City!"
   type="text"
@@ -17,6 +19,11 @@ export default {
       city: ''
       woeid: ''
   },
+  /*
+    Within the send data method, it would call the API to get the title of the city as well as the woeid and latt_long.
+    The woeid would be used to get information for conslidated_weather where it would tell you about the weather conditions in the city
+    latt_long would be used for the map feature to to place a marker on the map of the location of the city
+  */
   methods: {
       sendData: function() {
           var app = this
@@ -24,6 +31,7 @@ export default {
             .then(function(response) {
                 app.city = response.data.title
                 app.woeid = response.data.woeid
+                app.latt_long = response.data.latt_long
             })
             .catch(function (error) {
                 app.city = "Invalid City"
